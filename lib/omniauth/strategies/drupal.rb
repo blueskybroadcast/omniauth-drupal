687ed49b861cd558834d0df50c4aaba891e3742a
Eugene Correia <ecorreia@blueskybroadcast.com>
2014-09-25 13:31:49 -0700

    Add member status from drupal

--- lib/omniauth/strategies/drupal.rb
+++ lib/omniauth/strategies/drupal.rb
@@ -1,26 +1,28 @@
 require 'omniauth-oauth2'
 
-module Omniauth
+module OmniAuth
   module Strategies
     class Drupal < OmniAuth::Strategies::OAuth2
+      attr_accessor :session_id, :session_name
+
       option :client_options, {
         site: 'http://ncs-civicrm.prometdev.com',
         authorize_url: '/user',
-        authenticate_url: '/i4a/api/authenticate',
-        user_info_url: '/i4a/api/json/view.ams_contactInformation_memberType',
+        authenticate_url: '/api/remote_login/user/login',
+        user_info_url: '/api/remote_login/sso/retriever',
         username: 'MUST BE SET',
         password: 'MUST BE SET'
       }
 
-      uid { member_data[member_columns.find_index('ID')] }
+      uid { member_id }
 
-      name {'bcom'}
+      name {'drupal'}
 
       info do
         {
-          first_name: member_data[member_columns.find_index('FIRSTNAME')],
-          last_name: member_data[member_columns.find_index('LASTNAME')],
-          email: member_data[member_columns.find_index('EMAIL')],
+          first_name: raw_info['first_name'],
+          last_name: raw_info['last_name'],
+          email: raw_info['email'],
           is_member: is_member
         }
       end
@@ -43,9 +45,13 @@ module Omniauth
           response = authenticate
 
           if response.success?
+            response_body = JSON.parse(response.body)
             self.access_token = {
-              :token => JSON.parse(response.body)['authkey']
+              :token => response_body['token']
             }
+
+            self.session_id = response_body['sessid']
+            self.session_name = response_body['session_name']
 
             self.env['omniauth.auth'] = auth_hash
             self.env['omniauth.origin'] = '/' + request.params['slug']
@@ -72,25 +78,30 @@ module Omniauth
       private
 
       def authenticate
-        Typhoeus.get("#{authenticate_url}/#{username}/#{password}/#{authentication_token}")
+        Typhoeus.post(authenticate_url, body: {
+          username: username,
+          password: password}
+        )
       end
 
       def authenticate_url
         "#{options.client_options.site}#{options.client_options.authenticate_url}"
       end
 
-      def authentication_token
-        options.client_options.authentication_token
-      end
-
       def get_user_info(token, member_id)
-        response = Typhoeus.get("#{user_info_url}/contactid=#{member_id}/#{token}")
+        response = Typhoeus.post(user_info_url,
+          body: { uid: member_id },
+          headers: {'Cookie' => "#{session_name}=#{session_id}", 'X-CSRF-Token' => token})
 
         if response.success?
           JSON.parse(response.body)
         else
           nil
         end
+      end
+
+      def is_member
+        raw_info['acct_status'] == '1'
       end
 
       def member_id
@@ -107,20 +118,6 @@ module Omniauth
 
       def user_info_url
         "#{options.client_options.site}#{options.client_options.user_info_url}"
-      end
-
-      def is_member
-        !member_data[member_columns.find_index('MEMBERTYPEID')].nil? &&
-          member_data[member_columns.find_index('MEMBERTYPEID')] > 0 &&
-          Date.parse(member_data[member_columns.find_index('PAIDTHRU')]) >= Date.today
-      end
-
-      def member_columns
-        raw_info['COLUMNS']
-      end
-
-      def member_data
-        @member_data ||= raw_info['DATA'].flatten
       end
     end
   end

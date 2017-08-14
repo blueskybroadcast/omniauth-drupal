ba0eea52ef9663c6f3072226a0fa2f546af63cc8
Max Grechko <gremax@gremax.me>
2017-08-14 13:37:01 +0300

    App Events Support (integration logs)

--- lib/omniauth/strategies/drupal.rb
+++ lib/omniauth/strategies/drupal.rb
@@ -5,6 +5,8 @@ module OmniAuth
     class Drupal < OmniAuth::Strategies::OAuth2
       attr_accessor :session_id, :session_name
 
+      option :app_options, { app_event_id: nil }
+
       option :client_options, {
         site: 'http://ncs-civicrm.prometdev.com',
         authorize_url: '/user',
@@ -42,6 +44,9 @@ module OmniAuth
       end
 
       def callback_phase
+        slug = request.params['slug']
+        @app_event = account.app_events.where(id: options.app_options.app_event_id).first_or_create(activity_type: 'sso')
+
         if member_id
           response = authenticate
 
@@ -55,12 +60,17 @@ module OmniAuth
             self.session_name = response_body['session_name']
 
             self.env['omniauth.auth'] = auth_hash
-            self.env['omniauth.origin'] = '/' + request.params['slug']
+            self.env['omniauth.origin'] = '/' + slug
+            self.env['omniauth.app_event_id'] = @app_event.id
+            finalize_app_event
             call_app!
           else
+            @app_event.fail!
             fail!(:invalid_credentials)
           end
         else
+          @app_event.logs.create(level: 'error', text: 'Invalid credentials')
+          @app_event.fail!
           fail!(:invalid_credentials)
         end
       end
@@ -78,22 +88,47 @@ module OmniAuth
 
       private
 
+      def app_event_log_response(callee, response)
+        response_log = "Drupal Authentication Response ##{callee} (code: #{response&.code}):\n#{response.inspect}"
+        log_level = response.success? ? 'info' : 'error'
+        @app_event.logs.create(level: log_level, text: response_log)
+      end
+
       def authenticate
-        Typhoeus.post(authenticate_url, body: {
+        request_log = "Drupal Authentication Request:\nPOST #{authenticate_url}"
+        @app_event.logs.create(level: 'info', text: request_log)
+        response = Typhoeus.post(authenticate_url, body: {
           username: username,
           password: password}
         )
+        app_event_log_response(__callee__, response)
+        response
       end
 
       def authenticate_url
         "#{options.client_options.site}#{options.client_options.authenticate_url}"
       end
 
+      def finalize_app_event
+        app_event_data = {
+          user_info: {
+            uid: uid,
+            first_name: info[:first_name],
+            last_name: info[:last_name],
+            email: info[:email]
+          }
+        }
+
+        @app_event.update(raw_data: app_event_data)
+      end
+
       def get_user_info(token, member_id)
+        request_log = "Drupal Authentication Request:\nPOST #{user_info_url}, token: #{token}"
+        @app_event.logs.create(level: 'info', text: request_log)
         response = Typhoeus.post(user_info_url,
           body: { uid: member_id },
           headers: {'Cookie' => "#{session_name}=#{session_id}", 'X-CSRF-Token' => token})
-
+        app_event_log_response(__callee__, response)
         if response.success?
           JSON.parse(response.body)
         else
